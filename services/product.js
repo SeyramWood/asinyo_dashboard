221885831e721a82bca2da75da9feb5323770514
SeyramWood <prikahaeyram@gmail.com>
2022-05-23 16:31:07 +0100

    Changed package manager from npm to yarn

--- services/product.js
+++ services/product.js
@@ -3,9 +3,12 @@ import Axios from './axios'
 export async function fetchProductCatMajors() {
   try {
     const { data } = await Axios(`/products/majors`)
-    return data.data
+    return data.data??[]
 
   } catch (error) {
+    
+    console.log(error);
+   
     throw Error("Could not fetch products category majors!")
   }
 }
